e0135551073622f9483795872398be149fbd3dce
vxshxk <hmmvishak@gmail.com>
2025-12-19 15:24:35 +0530

    initialize cobra cli

---
+++ app/cmd/root.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 TORIS
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "hello",
+	Short: "TORIS - Terminal Organized and Rational IntelliSense",
+	Long: `TORIS is an AI-powered terminal assistant designed to enhance your command-line experience.
+With TORIS, you can get intelligent suggestions, automate tasks, and streamline your workflow directly from the terminal.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hello.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
